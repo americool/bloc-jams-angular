a58f92cfd6225775f801a2a72e8b8a8fe026e62a
Americool <aberanderson@gmail.com>
2016-07-07 16:44:48 -0400

    finish checkpoint8

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,14 +1,24 @@
 (function() {
-    function SongPlayer() {
+    function SongPlayer(Fixtures) {
         var SongPlayer = {};
-        
-        
+/**
+* @desc Object contating album information
+* @type {Object}
+*/        
+        var currentAlbum = Fixtures.getAlbum(); 
 /**
 * @desc Buzz object audio file
 * @type {Object}
 */
         var currentBuzzObject = null;
-        
+/**
+* @function setSong
+* @desc returns index (number) of song
+* @param {Object} song
+*/
+        var getSongIndex = function(song){
+            return currentAlbum.songs.indexOf(song)
+        };
 /**
 * @function setSong
 * @desc Stops currently playing song and loads new audio file as currentBuzzObject
@@ -27,7 +37,11 @@
             });
             SongPlayer.currentSong = song; 
         };
-        
+/**
+* @function playSong
+* @desc Plays selected song and marks song.playing as true 
+* @param {Object} song
+*/
         var playSong = function(song){
             currentBuzzObject.play(); 
             song.playing = true; 
@@ -36,6 +50,7 @@
         SongPlayer.currentSong = null;
         
         SongPlayer.play = function(song) {
+            song = song || SongPlayer.currentSong
             if (SongPlayer.currentSong !== song){
                 setSong(song);
                 playSong(song);
@@ -49,9 +64,28 @@
         };
         
         SongPlayer.pause = function(song) {
+            song = song || SongPlayer.currentSong;
             currentBuzzObject.pause(); 
             song.playing = false; 
         };
+/**
+* @function SongPlayer.previous
+* @desc Gets current song index, then reduces by 1. If index is less than zero - stops music and sets the currently playing song to null, else it uses the current index to set the new song and plays it and updates appropriate variables. 
+*/        
+        SongPlayer.previous = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex--; 
+            
+            if (currentSongIndex < 0 ) {
+                currentBuzzObject.stop(); 
+                SongPlayer.currentSong.playing = null; 
+            }
+            else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
+        }
         
         return SongPlayer; 
     }
